a878517080bb5a639bc5aeeb1b3fad306eca3722
Ky Lucero <kileelucero@gmail.com>
2021-01-07 13:50:20 -0700

    Initial Setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const routes = require("./routes/index");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use("/", routes);
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log("PORT: " + PORT);
+});
